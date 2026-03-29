12f4f1064488f60b282e90695397f84edeabcf68
Iryna Nychyporenko <i.v.nichiporenko25@gmail.com>
2026-03-29 17:04:32 +0200

    fix: remove broken define and reset config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,6 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // ВАЖНО: Это просто инструкция для сборщика заменить одну строку на другую
-    "http://localhost:3333": JSON.stringify(""),
-  },
 });
