c0ccbbe0a991b5ed1fd131e8f3d622304a06385c
Iryna Nychyporenko <i.v.nichiporenko25@gmail.com>
2026-03-29 16:44:08 +0200

    fix: vite define syntax

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,4 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.API_URL": JSON.stringify(
-      process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3333",
-    ),
-    "global.BASE_URL": JSON.stringify(
-      process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3333",
-    ),
-  },
 });
