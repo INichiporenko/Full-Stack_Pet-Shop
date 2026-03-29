507e6097185ff3d52838c73a8eab1d2dcacf7770
Iryna Nychyporenko <i.v.nichiporenko25@gmail.com>
2026-03-29 16:10:16 +0200

    fix: automatic api path replacement for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "http://localhost:3333": JSON.stringify(
+      process.env.NODE_ENV === "production" ? "/api" : "http://localhost:3333",
+    ),
+  },
+});
